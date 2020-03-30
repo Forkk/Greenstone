2301f845a3c2bc117fb295f807d87448b9cd4cad
Forkk <contact@forkk.net>
2020-03-30 17:12:09 -0500

    Implement text wrapping in terminal widget

--- src/main/kotlin/net/forkk/greenstone/computer/WTerminal.kt
+++ src/main/kotlin/net/forkk/greenstone/computer/WTerminal.kt
@@ -2,6 +2,9 @@ package net.forkk.greenstone.computer
 
 import io.github.cottonmc.cotton.gui.client.ScreenDrawing
 import io.github.cottonmc.cotton.gui.widget.WWidget
+import net.minecraft.client.MinecraftClient
+
+private const val LINE_HEIGHT = 12
 
 class WTerminal(initMsg: String) : WWidget() {
     /** Contains the contents of the screen. */
@@ -30,8 +33,28 @@ class WTerminal(initMsg: String) : WWidget() {
         ScreenDrawing.coloredRect(x, y, width, height, 0xFF000000.toInt())
         var texty = y + height
         for (line in screenBuf.lines().reversed()) {
-            ScreenDrawing.drawString(line, x, texty, 0xFFFFFF)
-            texty -= 12
+            texty = paintLine(x, texty, line)
         }
+    }
+
+    /**
+     * Paints a line of output on the terminal, with wrapping.
+     *
+     * Lines are painted bottom to top, and this returns the y position of the top of the line.
+     */
+    private fun paintLine(x: Int, starty: Int, str: String): Int {
+        var y = starty
+        val lines = MinecraftClient.getInstance().textRenderer.wrapStringToWidthAsList(str, width)
+        for (line in lines.reversed()) {
+            ScreenDrawing.drawString(line, x, y, 0xFFFFFF)
+            y -= LINE_HEIGHT
+        }
+        return y
+    }
+
+    /** Determine how many lines a given line will need for wrapping. */
+    private fun lineWrapCount(line: String): Int {
+        val lineWidth = MinecraftClient.getInstance().textRenderer.getStringWidth(line)
+        return lineWidth / width
     }
 }
